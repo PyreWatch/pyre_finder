e9ace0286aa339c484d6667d4810a0456ee37778
Paolo Garcia <paologarcia002@outlook.com>
2021-03-12 20:04:02 -0500

    added Fighter model - validation for latitude and longitude - add map to admin view - added model to database

--- map/models.py
+++ map/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+class Fighter(models.Model):
+    FIRE_CHOICES = (
+        ('P', "Fire is Present"),
+        ('N', "Fire is not Present")
+    )
+    
+    latitude = models.DecimalField(max_digits=4, decimal_places=2, validators=[MaxValueValidator(90.00), MinValueValidator(-90.00)])
+    longitude = models.DecimalField(max_digits=5, decimal_places=2, validators=[MaxValueValidator(180.00), MinValueValidator(-180.00)])
+    status = models.CharField(max_length=1, choices = FIRE_CHOICES)
