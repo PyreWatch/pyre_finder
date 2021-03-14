d73e3714686b53b1ea3811c761d6f50cc191fe0f
Paolo Garcia <paologarcia002@outlook.com>
2021-03-13 19:54:58 -0500

    adds markers to map map refreshes ever 5 minutes

--- map/views.py
+++ map/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from pyrefinder.settings import env, ENV_DIR
+from .models import Fighter
 
 # Create your views here.
 def overall_map_view(request):
     mapbox_access_token = str(env("MAPBOX_API_KEY"))
-    return render(request, "mapindex.html", {'mapbox_access_token': mapbox_access_token })
+
+
+    context = {
+        'mapbox_access_token': mapbox_access_token,
+        'fighters': Fighter.objects.all()
+    }
+
+    return render(request, "mapindex.html", context)
